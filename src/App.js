b91eb0146696a01113e85945d1d8c4724eb00c61
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2022-06-01 23:53:47 +0200

    feat:add from / redirection to an userpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
+        <Route path="/" element={<Navigate to="/user/12" />} />
         <Route path="/user/:id" element={<Dashboard />} />
       </Routes>
     </div>
